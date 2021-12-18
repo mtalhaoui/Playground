04b799b8fdec764651a76020a222e3ed797fccfb
mtalhaoui <mohammed.talhaoui@hotmail.com>
2021-12-18 15:34:43 +0100

    Draw rectangle while pressing mouse

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, Subject, takeUntil } from 'rxjs';
+
 import { Rectangle } from '../shared/model/rectangle.model';
 
 @Component({
@@ -8,48 +10,46 @@ import { Rectangle } from '../shared/model/rectangle.model';
 })
 export class CanvasComponent implements OnInit {
   rectangles: Rectangle[] = [];
-
+  drawIcons: DrawIcon[] = [];
+  currentRectangle!: Rectangle;
   mouseDown: boolean = false;
-
-  lastMouseX!: number;
-  lastMouseY!: number;
   mouseX!: number;
   mouseY!: number;
+  lastMouseX!: number;
+  lastMouseY!: number;
   width!: number;
   height!: number;
-  rightToLeft!: boolean;
+  top!: number;
 
-  constructor() { }
+  constructor() {
+    this.currentRectangle = this.emptyRectangle();
+    this.resetState();
+  }
 
   ngOnInit(): void {
+    this.drawIcons = [
+      new DrawIcon("MoveIcon", "../../assets/move-icon.svg", false),
+      new DrawIcon("ArrowPointer", "../../assets/arrow-pointer.svg", true)
+    ]
   }
 
   onMouseDown(e: MouseEvent) {
     this.mouseDown = true;
-
     this.lastMouseX = e.offsetX;
     this.lastMouseY = e.offsetY;
   }
 
   onMouseUp(e: MouseEvent) {
-    this.mouseDown = false;
-
-    let left = 0, top = 0;
-
-    left = this.lastMouseX;
-    top = this.lastMouseY;
-
-    if (this.width < 0) {
-      this.width *= -1;
-      left = this.mouseX;
-    }
-
-    if (this.height < 0) {
-      this.height *= -1;
-      top = this.mouseY;
-    }
-
-    this.rectangles.push(new Rectangle(this.width, this.height, left, top));
+    let finalise = new Subject<void>();
+    this.getRectangle()
+      .pipe(takeUntil(finalise))
+      .subscribe((rectangle: Rectangle) => {
+        this.rectangles.push(rectangle);
+        this.currentRectangle = this.emptyRectangle();
+        this.resetState();
+        finalise.next();
+        finalise.complete();
+      });
   }
 
   onMouseMove(e: MouseEvent) {
@@ -59,6 +59,59 @@ export class CanvasComponent implements OnInit {
     if (this.mouseDown) {
       this.width = this.mouseX - this.lastMouseX;
       this.height = this.mouseY - this.lastMouseY;
+
+      let finalise = new Subject<void>();
+      this.getRectangle()
+        .pipe(takeUntil(finalise))
+        .subscribe((rectangle: Rectangle) => {
+          this.currentRectangle = rectangle;
+          finalise.next();
+          finalise.complete();
+        });
     }
   }
+
+  private getRectangle(): Observable<Rectangle> {
+    let left = this.lastMouseX, top = this.lastMouseY, width = this.width, height = this.height;
+
+    if (this.top === 0) {
+      this.top = top;
+    }
+
+    if (this.width < 0) {
+      width *= -1;
+      left = this.mouseX;
+    }
+
+    if (this.height < 0) {
+      height *= -1;
+      top = this.mouseY;
+    }
+
+    if (this.top === top) {
+      console.log("width: ", width, " height: ", height, " left: ", left, " top: ", this.top);
+      return of(new Rectangle(width, height, left, this.top));
+    } else {
+      return of(this.emptyRectangle());
+    }
+  }
+
+  private emptyRectangle(): Rectangle {
+    return new Rectangle(0, 0, 0, 0);
+  }
+
+  resetState(): void {
+    this.mouseDown = false;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.lastMouseX = 0;
+    this.lastMouseY = 0;
+    this.width = 0;
+    this.height = 0;
+    this.top = 0;
+  }
+}
+
+export class DrawIcon {
+  constructor(public name: string, public path: string, public active: boolean) { }
 }
